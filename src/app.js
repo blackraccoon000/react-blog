6a71ceec7eb7774e448a9d2f113701e917dc1613
Yutaka Fujii <yutakaf@playwell.site>
2021-03-24 20:19:44 +0900

    adding webpack serve

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-import "./styles/style.css"
+import "./styles/styles.scss"
 import Icon from "./img/favicon.png"
 
-const component = () => {
-  // console.log("test")
-  const myIcon = new Image()
-  myIcon.src = Icon
+// const component = () => {
+//   // console.log("test")
+//   const myIcon = new Image()
+//   myIcon.src = Icon
 
-  const element = document.createElement("div")
-  element.appendChild(myIcon)
+//   const element = document.createElement("div")
+//   element.appendChild(myIcon)
 
-  return element
-}
+//   return element
+// }
 
-// test()
-document.body.appendChild(component())
+// // test()
+// document.body.appendChild(component())
 
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const jsx = (
+  <div>
+    <h1>Hello World</h1>
+    <h2>Good to see you here we go!</h2>
+  </div>
+)
+
+ReactDOM.render(jsx,document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path")
 
+const Mode = "development"
+const enableSourceMap = Mode === "development"
+
 module.exports = (env) => {
   return {
-    mode: "development",
+    mode: Mode,
     entry: "./src/app.js",
     output: {
       path: path.join(__dirname,"public","dist"),
@@ -11,14 +14,43 @@ module.exports = (env) => {
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ["style-loader","css-loader"]
+          test: /\.js$/,
+          loader:"babel-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/i,
+          // use: ["style-loader","css-loader","sass-loader"]
+          use: [
+            {
+              loader:"style-loader"
+            },
+            {
+              loader:"css-loader",
+              options:{
+                sourceMap:true
+              }
+            },
+            {
+              loader:"sass-loader",
+              options:{
+                sourceMap:true
+              }
+            },
+          ]
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: "asset/resource"
         }
       ]
+    },
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      port: 8081,
+      publicPath: "/dist/"
     }
   }
 }
