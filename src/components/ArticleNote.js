6286f1a826e2cead667cfd25e5a5a76474aa808b
Yutaka Fujii <yutakaf@playwell.site>
2021-04-14 16:33:52 +0900

    create article page

--- src/components/ArticleNote.js
+++ src/components/ArticleNote.js
@@ -1,29 +1,25 @@
-import React from "react"
-import { DateTime } from "luxon"
+import React from 'react';
+import { DateTime } from 'luxon';
 
 const ArticleNote = (props) => {
   return (
-    <div className="article-item">
+    <article className="article-item">
       <figure className="article-item__photo">
-        { props.link ?
-          <img
-            className="article-item__img"
-            src={props.link}
-            alt="dog"
-          />
-        :
-          <div className="article-item__imgDiv">
-            <p className="article-item__imgDiv__p">No Image</p>
-          </div>
-        }
+        {props.link ? (
+          <img className="article-item__img" src={props.link} alt="dog" />
+        ) : (
+          <div className="article-item__imgDiv">No Image</div>
+        )}
       </figure>
       <div className="article-item__box">
         <h3 className="article-item__title">{props.title}</h3>
         <p className="article-item__text">{props.note}</p>
-        <p className="article-item__date">{DateTime.fromMillis(props.createdAt).toFormat('yyyy LL dd')}</p>
+        <p className="article-item__date">
+          {DateTime.fromMillis(props.createdAt).toFormat('yyyy LL dd')}
+        </p>
       </div>
-    </div>
-  )
-}
+    </article>
+  );
+};
 
-export default ArticleNote
+export default ArticleNote;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,35 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { connect } from "react-redux"
-import ArticleNote from "./ArticleNote"
-import getVisibleBlogData from "../selectors/getVisibleBlogData"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ArticleNote from './ArticleNote';
+import getVisibleBlogData from '../selectors/getVisibleBlogData';
 
 const Home = (props) => {
   return (
-    <div className="content-container">
-      {
-        props.blogData.map(note => {
-          return props.isAuthenticated ?
-          <Link key={`link_${note.id}`} to={`/edit/${note.id}`}>
+    <div className="contents-wrapper">
+      {props.blogData.map((note) => {
+        return props.isAuthenticated ? (
+          <Link
+            key={`link_${note.id}`}
+            to={`/edit/${note.id}`}
+            className="article-link"
+          >
             <ArticleNote key={note.id} {...note} />
           </Link>
-          :
+        ) : (
           <ArticleNote key={note.id} {...note} />
-        })
-      }
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state)=>{
+const mapStateToProps = (state) => {
   return {
-    blogData: getVisibleBlogData(state.blogData,state.filters),
-    isAuthenticated: !!state.auth.uid
-  }
-}
+    blogData: getVisibleBlogData(state.blogData, state.filters),
+    isAuthenticated: !!state.auth.uid,
+  };
+};
 
-export default connect(mapStateToProps)(Home)
-export { Home }
+export default connect(mapStateToProps)(Home);
+export { Home };
